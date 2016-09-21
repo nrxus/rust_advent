76c1dd23fb679a9b588248bc66ef9895bad6d795
Andres <andresnrx@gmail.com>
2016-09-20 20:13:31 -0700

    split first problem into its own module in its library

--- src/main.rs
+++ src/main.rs
@@ -1,45 +1,7 @@
-use std::io::{self, Read};
 use std::env;
 
-fn count(input: &str) -> i32 {
-    let mut answer = 0;
-    for c in input.chars() {
-        answer = if c == '(' {
-            answer + 1
-        } else if c == ')' {
-            answer - 1
-        } else {
-            answer
-        }
-    }
-    answer
-}
-
-fn find_basement(input: &str) -> i32 {
-    let mut answer = 0;
-    let mut count = 0;
-    for c in input.chars() {
-        answer += 1;
-        count = if c == '(' {
-            count + 1
-        } else if c == ')' {
-            count - 1
-        } else {
-            count
-        };
-        if count == -1 {
-           break;
-        }
-    }
-    answer
-}
-
-fn one() {
-    let mut input = String::new();
-    io::stdin().read_to_string(&mut input).expect("Failed to read input");
-    let answer = find_basement(&input);
-    println!("{}", answer);
-}
+extern crate advent_of_code;
+use advent_of_code::*;
 
 fn main() {
     if env::args().len() != 2 {
@@ -50,9 +12,8 @@ fn main() {
     let problem_number: u32 =
         problem_number.trim().parse().expect("Please type a number for the problem (i.e., 1)!");
     match problem_number {
-        1 => one(),
+        1 => one::solve(),
         2...25 => println!("Not yet implemented ☹"),
         _ => println!("This problem number does not exist!"),
     }
-    return;
 }
---
+++ src/one.rs
@@ -0,0 +1,43 @@
+use std::io::{self, Read};
+
+pub fn solve() {
+    let mut input = String::new();
+    io::stdin().read_to_string(&mut input).expect("Failed to read input");
+    let count = count(&input);
+    let position = find_basement(&input);
+    println!("1A: {}", count);
+    println!("1B: {}", position);
+}
+
+fn count(input: &str) -> i32 {
+    let mut count = 0;
+    for c in input.chars() {
+        count = if c == '(' {
+            count + 1
+        } else if c == ')' {
+            count - 1
+        } else {
+            count
+        }
+    }
+    count
+}
+
+fn find_basement(input: &str) -> i32 {
+    let mut answer = 0;
+    let mut count = 0;
+    for c in input.chars() {
+        answer += 1;
+        count = if c == '(' {
+            count + 1
+        } else if c == ')' {
+            count - 1
+        } else {
+            count
+        };
+        if count == -1 {
+            break;
+        }
+    }
+    answer
+}
