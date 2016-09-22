78f05e67ff93871ad1badb23e2d556d9e62b9e78
Andres Medina <amedina@pivotal.io>
2016-09-22 14:11:23 -0700

    refactored; but still ugly

--- src/solvers/three.rs
+++ src/solvers/three.rs
@@ -10,24 +10,24 @@ pub fn solve(input: &str) -> Answer {
 }
 
 fn num_houses(input: &str, num: usize) -> usize {
-    if num == 0 {
-        return 0;
-    }
-
-    let origin = House { x: 0, y: 0 };
-
     let mut santas = Vec::with_capacity(num);
 
     for _ in 0..num {
-        santas.push(Santa::new(origin));
+        santas.push(Santa::new(House { x: 0, y: 0 }));
     }
 
+    houses_visited(input, &mut santas).len()
+}
+
+fn houses_visited(input: &str, santas: &mut Vec<Santa>) -> HashSet<House> {
     let mut houses = HashSet::new();
-    houses.insert(origin);
+    for santa in &*santas {
+        houses.insert(santa.house);
+    }
 
     let mut iter = input.chars();
     'outer: loop {
-        for santa in &mut santas {
+        for santa in &mut *santas {
             let c = iter.next();
             let c = match c {
                 Some(v) => v,
@@ -39,7 +39,7 @@ fn num_houses(input: &str, num: usize) -> usize {
         }
     }
 
-    houses.len()
+    houses
 }
 
 struct Santa {
